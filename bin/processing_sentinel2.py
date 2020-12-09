4d6cf3de78c1010f55c57dacda329f9c13f65f40
UrielMendoza <46738566+UrielMendoza@users.noreply.github.com>
2020-12-09 16:08:21 -0600

    Update processing_sentinel2.py

--- bin/processing_sentinel2.py
+++ bin/processing_sentinel2.py
@@ -196,7 +196,7 @@ def pixelNubesBajas(dsRef,dsSar):
     # Valor de referencia B4
     nubeBaja = 900
 
-	for i in range(nuMask.shape[0]):
+    for i in range(nuMask.shape[0]):
 		for j in range(nuMask.shape[1]):
 			#print(nuMask.shape[0],nuMask.shape[1])
 			#print('pocision:',i,j)
