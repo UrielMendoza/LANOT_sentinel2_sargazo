4d2540a52d3b0456f933f8f9a47bf209cb202348
UrielMendoza <urielmendozacastillo@gmail.com>
2021-08-09 21:32:27 -0500

    Update processing.py

--- bin/processing.py
+++ bin/processing.py
@@ -316,11 +316,11 @@ def sargazoL2A(pathInput,pathOutput,pathTmp,pathLM,pathOutputEmpty,pathOutputGeo
 
 #                finally:
                 # BORRA BASURA
-                    #os.system('rm -r '+pathTmp+'*.tif')
-                    #os.system('rm -r '+pathTmp+'*.geojson')
-                    #os.system('rm -r '+pathTmp+'*.csv')
-                    #os.system('rm -r '+pathTmp+'*.json')
-                    #os.system('rm -r '+pathTmp+'*.SAFE')
+                    os.system('rm -r '+pathTmp+'*.tif')
+                    os.system('rm -r '+pathTmp+'*.geojson')
+                    os.system('rm -r '+pathTmp+'*.csv')
+                    os.system('rm -r '+pathTmp+'*.json')
+                    os.system('rm -r '+pathTmp+'*.SAFE')
                 
             else:
                 print('Archivo: '+archivo+' ya fue procesado')
