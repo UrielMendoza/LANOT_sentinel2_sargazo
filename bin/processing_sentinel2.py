fb565a2405540529bb7cf132f5bd1b8f81110d22
UrielMendoza <urielmendozacastillo@gmail.com>
2021-08-10 23:29:08 -0500

    Update processing_sentinel2.py

--- bin/processing_sentinel2.py
+++ bin/processing_sentinel2.py
@@ -642,12 +642,12 @@ def conexionDB():
 
 def insertSargazoDB(conect,cur,crs,pathInput):
     print('Añadiendo a DB')
-	with open(pathInput, 'r') as f:
-		reader = csv.reader(f)
-		next(reader)
+    with open(pathInput, 'r') as f:
+        reader = csv.reader(f)
+        next(reader)
         for row in reader:
 			#print('Añadiendo a DB: ', row)
-			cur.execute("INSERT INTO sargazo VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, ST_Transform(ST_GeomFromText(%s,"+crs+"),4326))", row)		
+            cur.execute("INSERT INTO sargazo VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, ST_Transform(ST_GeomFromText(%s,"+crs+"),4326))", row)		
 	cur.execute("SELECT * from sargazo")
 	row = cur.fetchall()
 	conect.commit()
