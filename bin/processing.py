acf13f07052a4177b315732390a020e67f6fcad6
UrielMendoza <urielmendozacastillo@gmail.com>
2021-08-20 02:06:04 -0500

    Update processing.py

--- bin/processing.py
+++ bin/processing.py
@@ -147,11 +147,11 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathOutputEmpty,
         # DESCARGA
         print('1. Descargando...')
         print('Sentinel-2\nInicio:',start_date,'\nTermino:',end_date)
-        download_datasets.search_and_download_datasets(tiles, start_date, end_date, pathTmp, unzip=False)
+        #download_datasets.search_and_download_datasets(tiles, start_date, end_date, pathTmp, unzip=False)
 
         # Reste dias para prueba
         #print('Sentinel-2\nInicio:',start_date-datetime.timedelta(days=2),'\nTermino:',end_date-datetime.timedelta(days=2))
-        #download_datasets.search_and_download_datasets(tiles, start_date - datetime.timedelta(days=2), end_date - datetime.timedelta(days=2), pathTmp, unzip=False)
+        download_datasets.search_and_download_datasets(tiles, start_date - datetime.timedelta(days=1), end_date - datetime.timedelta(days=1), pathTmp, unzip=False)
         #except AttributeError:
         #    print('Error de descarga')
         #    pass
