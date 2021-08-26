8753ce8a299f661bac43c5c9c52ec7a87b46d4e0
UrielMendoza <urielmendozacastillo@gmail.com>
2021-08-26 17:24:19 -0500

    prueba error descarga

--- bin/processing.py
+++ bin/processing.py
@@ -149,7 +149,7 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathOutputEmpty,
             # DESCARGA
             print('1. Descargando...')
             print('Sentinel-2\nInicio:',start_date,'\nTermino:',end_date)
-            download_datasets.search_and_download_datasets('error', start_date, end_date, pathTmp, unzip=False)
+            download_datasets.search_and_download_datasets(tiles, 'error', end_date, pathTmp, unzip=False)
 
             # Reste dias para prueba
             #print('Sentinel-2\nInicio:',start_date-datetime.timedelta(days=2),'\nTermino:',end_date-datetime.timedelta(days=2))
