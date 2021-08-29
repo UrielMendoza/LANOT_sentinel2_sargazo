a193739c81f9fa5b181d8010272fc14716db4b1f
UrielMendoza <urielmendozacastillo@gmail.com>
2021-08-28 19:31:18 -0500

    Update processing.py

--- bin/processing.py
+++ bin/processing.py
@@ -195,7 +195,6 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathOutputEmpty,
                 dirI = processing_sentinel2.nomDir(archivo,'L2A')
                 print("Fecha: "+fecha)
                 print("Tile: "+tile)
-                print(tile[1:] in tiles)
             except Exception as e:
                 print('***Error en obtener datos***')
                 processing_sentinel2.agregaErrorSargazoDB(archivo,'',fecha,tile,traceback.format_exc().replace("'",""))
@@ -203,7 +202,7 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathOutputEmpty,
 
             # COMPRUEBA LOG Y TILES
             #if not processing_sentinel2.verificaLog(pathLog+nomLog,archivo):
-            if (processing_sentinel2.verificaSargazoDB(tile,fecha) == 0) and (tile[1:] in tiles == True):                   
+            if (processing_sentinel2.verificaSargazoDB(tile,fecha) == 0) and not(tile[1:] in tiles == True):                   
                 try:
                     #====================
                     # INICIA PROCESO
