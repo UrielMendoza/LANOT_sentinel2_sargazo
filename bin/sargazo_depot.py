31205d0da89f14108e65db3e8d848e6d9c433ee1
UrielMendoza <urielmendozacastillo@gmail.com>
2021-09-03 19:36:31 -0500

    Create sargazo_depot.py

---
+++ bin/sargazo_depot.py
@@ -0,0 +1,21 @@
+from glob import glob
+import os
+import sys
+from processing_sentinel2 import obtieneTile,obtieneAnio
+
+pathInput = '/data/input/sentinel2/depot/'
+pathOutput = '/data/input/sentinel2/L1C/'
+
+archivos = glob(pathInput+'*')
+
+for archivo in archivos:
+
+    print(archivo)
+    tile = obtieneTile(archivo)
+    anio = obtieneAnio(archivo)
+    print(tile)
+    print(anio)
+    if '.zip' in archivo:
+        os.system('mkdir -p '+pathOutput+tile+'/')
+        os.system('mv '+archivo+' '+pathOutput+tile+'/')
+
