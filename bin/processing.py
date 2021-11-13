b00948aa566eb1feb74ae35a91b59d57a3ba6c9a
UrielMendoza <urielmendozacastillo@gmail.com>
2021-11-13 10:51:54 -0600

    Update processing.py

--- bin/processing.py
+++ bin/processing.py
@@ -158,12 +158,12 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,path
             # DESCARGA
             print('1. Descargando...')
             print('Sentinel-2\nInicio:',start_date,'\nTermino:',end_date)
-            download_datasets.search_and_download_datasets(tiles, start_date, end_date, pathTmp, unzip=False)
+            #download_datasets.search_and_download_datasets(tiles, start_date, end_date, pathTmp, unzip=False)
 
             # Reste dias para prueba
             #print('Sentinel-2\nInicio:',start_date-datetime.timedelta(days=2),'\nTermino:',end_date-datetime.timedelta(days=2))
-            #daysDelta = 3
-            #download_datasets.search_and_download_datasets(tiles, start_date - datetime.timedelta(days=daysDelta), end_date - datetime.timedelta(days=daysDelta), pathInputL1C, unzip=False)
+            daysDelta = 1
+            download_datasets.search_and_download_datasets(tiles, start_date - datetime.timedelta(days=daysDelta), end_date - datetime.timedelta(days=daysDelta), pathInputL1C, unzip=False)
         except Exception as e:
             print('***Error en la descarga***')
             processing_sentinel2.agregaErrorSargazoDB('','',start_date.strftime('%Y%m%dT%H%M%S'),'',traceback.format_exc().replace("'",""))
