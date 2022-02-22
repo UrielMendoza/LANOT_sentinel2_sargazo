a1951e627b45974300e786595d11b7c409c13905
UrielMendoza <urielmendozacastillo@gmail.com>
2022-02-22 16:22:33 -0600

    Update processing_sentinel2.py

--- bin/processing_sentinel2.py
+++ bin/processing_sentinel2.py
@@ -801,8 +801,8 @@ def insertCatalogoDB(compuesto,conect,cur,crs,pathInput):
         next(reader)
         for row in reader:
 			#print('Añadiendo a DB: ', row)
-            cur.execute("INSERT INTO public.'catalogo_"+compuesto.upper()+"' VALUES (DEFAULT, ST_GeomFromText(%s,"+crs+"), %s, %s)", row)
-        cur.execute("SELECT * from catalogo_"+compuesto.upper())
+            cur.execute("INSERT INTO public.catalogo_"+compuesto.upper()+" VALUES (DEFAULT, ST_GeomFromText(%s,"+crs+"), %s, %s)", row)
+        cur.execute("SELECT * from public.catalogo_"+compuesto.upper())
     row = cur.fetchall()
     conect.commit()
 
