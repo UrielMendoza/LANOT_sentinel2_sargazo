755d3cb4bfaf5c6b641229c645c901a26d055c81
UrielMendoza <urielmendozacastillo@gmail.com>
2022-02-22 19:36:46 -0600

    Update processing_sentinel2.py

--- bin/processing_sentinel2.py
+++ bin/processing_sentinel2.py
@@ -821,7 +821,7 @@ def deleteSargazoLogDB(conect,cur,tile,fecha):
 
 def deleteCatalogoDB(conect,cur,nombre,compuesto):
     print('Borrando catalogo de DB: ')
-    cur.execute('DELETE FROM public."catalogo_'+compuesto.upper()+'" WHERE "location" = "'+nombre+'"')
+    cur.execute('DELETE FROM public."catalogo_'+compuesto.upper()+'" WHERE location = '+nombre+'')
     conect.commit()
 
 def insertSargazoLogDB(conect,cur,pathl2a,pathsargazo,fecha,tile,sargazo,totalsar,porcNube,tproc):
