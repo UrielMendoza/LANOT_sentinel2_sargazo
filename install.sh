8ec8c627b6a55206c504c4fa90bb08988dda9af4
UrielMendoza <urielmendozacastillo@gmail.com>
2022-03-12 17:40:41 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ mkdir -p ./test/tmp/manual/;
 mkdir -p ./test/tmp/semi_manual/;
 #wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/land_UTM16N_20m.geojson;
 #wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/land_UTM16N_20m_1.geojson;
-#wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/land_UTM16N_20m_2021.geojson;
+wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/land_UTM16N_20m_2021.geojson;
 wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/land_UTM16N_20m_SPlaya_2021.geojson;
 wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/playas_UTM16N_20m_2021.geojson;
 wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/land_UTM16N_20m_distance.geojson;
