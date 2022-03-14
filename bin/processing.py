6b2451eb8964f121728b93c46188f0775d1d9c3e
UrielMendoza <urielmendozacastillo@gmail.com>
2022-03-14 07:19:42 -0600

    prueba detfo cambio

--- bin/processing.py
+++ bin/processing.py
@@ -439,7 +439,7 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,path
 
                 finally:
                 # BORRA BASURA
-                    os.system('rm -r '+pathTmp+'*.tif')
+                    #os.system('rm -r '+pathTmp+'*.tif')
                     os.system('rm -r '+pathTmp+'*json')
                     os.system('rm -r '+pathTmp+'*.csv')
                     os.system('rm -r '+pathTmp+'*.zip')
@@ -487,7 +487,7 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,path
     # BORRA DIR DESCARGA
     # NO DESCOMENTAR EN SEMIMANUAL PORQUE BORRA IMAGENES
     if dateTime == 'automatico' or dateTime == 'manual':
-        os.system('rm -r '+pathTmp+'*')
+        #os.system('rm -r '+pathTmp+'*')
 
 
 
