1599a11944401a50ebbeb3f10be2ad27f4cee003
UrielMendoza <urielmendozacastillo@gmail.com>
2022-03-23 09:25:15 -0600

    prueba

--- bin/processing.py
+++ bin/processing.py
@@ -458,7 +458,7 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,path
 
                 finally:
                 # BORRA BASURA
-                    os.system('rm -r '+pathTmp+'*.tif')
+                    #os.system('rm -r '+pathTmp+'*.tif')
                     os.system('rm -r '+pathTmp+'*json')
                     os.system('rm -r '+pathTmp+'*.csv')
                     os.system('rm -r '+pathTmp+'*.zip')
@@ -505,8 +505,8 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,path
         processing_sentinel2.enviaMail(fecha,'mosaico',traceback.format_exc().replace("'",""))
     # BORRA DIR DESCARGA
     # NO DESCOMENTAR EN SEMIMANUAL PORQUE BORRA IMAGENES
-    if dateTime == 'automatico' or dateTime == 'manual':
-        os.system('rm -r '+pathTmp+'*')
+    #if dateTime == 'automatico' or dateTime == 'manual':
+        #os.system('rm -r '+pathTmp+'*')
 
 
 
