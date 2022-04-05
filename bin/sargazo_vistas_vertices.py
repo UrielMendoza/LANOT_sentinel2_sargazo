b932e86bb5e596641d356e027a3fa2553ed0e10e
UrielMendoza <urielmendozacastillo@gmail.com>
2022-04-05 13:05:28 -0500

    Update sargazo_vistas_vertices.py

--- bin/sargazo_vistas_vertices.py
+++ bin/sargazo_vistas_vertices.py
@@ -226,7 +226,7 @@ if __name__ == '__main__':
         print(ulx, uly, lrx, lry)
         
         im_in = Image.open(path)
-        height = 1200
+        height = 4200
         width = int(height * im_in.width / im_in.height)
         #im_in = im_in.resize((width, height)).convert('RGB')
         im_in = im_in.convert('RGB')
