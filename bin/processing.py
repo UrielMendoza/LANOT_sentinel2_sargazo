dc5508ddc80bb4877fb17df46a9e7e1f4e78b3cd
UrielMendoza <urielmendozacastillo@gmail.com>
2022-05-20 16:30:18 -0500

    Update processing.py

--- bin/processing.py
+++ bin/processing.py
@@ -462,7 +462,7 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,path
                     #os.system('rm -r '+pathTmp+'*json')
                     #os.system('rm -r '+pathTmp+'*.csv')
                     #os.system('rm -r '+pathTmp+'*.zip')
-                    #os.system('rm -r '+pathTmp+'*.SAFE')
+                    os.system('rm -r '+pathTmp+'*.SAFE')
                     #if dateTime == 'automatico' or dateTime == 'manual':
                     #    os.system('rm -r '+pathTmp+'*')
                 
@@ -506,7 +506,7 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,path
     # BORRA DIR DESCARGA
     # NO DESCOMENTAR EN SEMIMANUAL PORQUE BORRA IMAGENES
     #if dateTime == 'automatico' or dateTime == 'manual':
-    #    os.system('rm -r '+pathTmp+'*')
+    #   os.system('rm -r '+pathTmp+'*')
 
 
 
