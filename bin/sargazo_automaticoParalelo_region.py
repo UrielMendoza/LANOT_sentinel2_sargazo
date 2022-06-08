2b0434f38ed3c0e684d22c4dfa90f56ad7d7ec74
UrielMendoza <urielmendozacastillo@gmail.com>
2022-06-08 14:05:19 -0500

    Update sargazo_automaticoParalelo_region.py

--- bin/sargazo_automaticoParalelo_region.py
+++ bin/sargazo_automaticoParalelo_region.py
@@ -8,10 +8,11 @@ Created on Wen Jun 08 15:35:37 2022
 import os
 from multiprocessing import Pool
 
-os.system('export PATH=/home/lanotadm/LANOT_sentinel2_sargazo/bin:$PATH')
+#os.system('export PATH=/home/lanotadm/LANOT_sentinel2_sargazo/bin:$PATH')
+pathScript = '/home/lanotadm/LANOT_sentinel2_sargazo/bin/'
 
 # Script para obtencion de sargazo por tile
-script_1 = 'sargazo_automaticoParalelo_region.py'
+script_1 = pathScript+'sargazo_automaticoParalelo_region.py'
 
 # Regiones
 sargazo_3 = ("16QDJ","16QEJ","16QDH","16QEH","16QDG","16QEG","16QDF","16QEF","16QCF","16QCE","16QDE","16QEE","16QCD","16QDD","16QED","16PCC","16PDC","16PEC")
