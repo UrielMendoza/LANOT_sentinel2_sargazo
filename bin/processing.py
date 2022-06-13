6aa484f48b857085e6b3f001a236ee044a4af1f3
UrielMendoza <urielmendozacastillo@gmail.com>
2022-06-13 00:08:06 -0500

    Update processing.py

--- bin/processing.py
+++ bin/processing.py
@@ -28,8 +28,8 @@ def semiManual():
     return start_date,end_date,region,SNbuffer
 
 def automaticoParalelo():
-    # Se le resta un dia, porque el servidor en UTC
-    daysDelta = 2
+    # Se le resta un d3a, porque el servidor en UTC
+    daysDelta = 3
     start_date = datetime.datetime.now() - datetime.timedelta(days=daysDelta)
     end_date = datetime.datetime.now()  - datetime.timedelta(days=daysDelta)
     #region = "sargazo_1"
