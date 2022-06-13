9cb26a9a5dc1882196cec822133e865d5e99f339
UrielMendoza <46738566+UrielMendoza@users.noreply.github.com>
2022-06-13 13:49:46 -0500

    Update sargazo_vistas_vertices.py

--- bin/sargazo_vistas_vertices.py
+++ bin/sargazo_vistas_vertices.py
@@ -226,6 +226,7 @@ if __name__ == '__main__':
         path = pathTmp+"tmp.tif"
         get_limits(path)
         print(ulx, uly, lrx, lry)
+        print(path)
         
         im_in = Image.open(path)
         height = 1200
