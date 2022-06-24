57b7a4e63aa7f125172639b2a582152ca78a4eea
UrielMendoza <urielmendozacastillo@gmail.com>
2022-06-24 13:46:13 -0500

    Update sargazo_paraleloMosaico.py

--- bin/sargazo_paraleloMosaico.py
+++ bin/sargazo_paraleloMosaico.py
@@ -30,12 +30,12 @@ script_1 = 'python3 '+pathScript+'sargazo_automatico_imagenTile.py'
 
 # Regiones
 #prueba = ("16QDJ","16QEJ")
-sargazo_1 = ("16QDJ","16QEJ","16QDH","16QEH","16QDG","16QEG","16QDF","16QEF")
+#sargazo_1 = ("16QDJ","16QEJ","16QDH","16QEH","16QDG","16QEG","16QDF","16QEF")
 #sargazo_2 = ("16QCF","16QCE","16QDE","16QEE","16QCD","16QDD","16QED","16PCC","16PDC","16PEC")
-#argazo_3 = ("16QDJ","16QEJ","16QDH","16QEH","16QDG","16QEG","16QDF","16QEF","16QCF","16QCE","16QDE","16QEE","16QCD","16QDD","16QED","16PCC","16PDC","16PEC")
+sargazo_3 = ("16QDJ","16QEJ","16QDH","16QEH","16QDG","16QEG","16QDF","16QEF","16QCF","16QCE","16QDE","16QEE","16QCD","16QDD","16QED","16PCC","16PDC","16PEC")
 #=========================================================================================================
 processes = []
-for i in sargazo_1:
+for i in sargazo_3:
     processes.append(script_1+' '+i)
 #=========================================================================================================
 
