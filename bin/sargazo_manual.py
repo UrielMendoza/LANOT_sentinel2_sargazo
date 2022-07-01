dfaec9598f8abccf5b0fa5799585c8c219f83d9e
UrielMendoza <urielmendozacastillo@gmail.com>
2022-07-01 13:32:27 -0500

    Update sargazo_manual.py

--- bin/sargazo_manual.py
+++ bin/sargazo_manual.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
             descargaImagenes(pathTmp,dateTime='manual')
             imagenL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,pathOutputEmpty,pathOutputGeoTiff,pathOutputWeb,pathOutputPeta,pathInputPeta,pathVertices,pathLog,pathLanot,pathOutputVistas,dateTime='manual')
             mosaicoL2A(pathTmp,pathInputL1C,pathOutputGeoTiff,pathOutputPeta,pathOutputWeb,dateTime='automaticoTile')
-            #sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,pathOutputEmpty,pathOutputGeoTiff,pathOutputWeb,pathOutputPeta,pathInputPeta,pathVertices,pathLog,pathLanot,pathOutputVistas,dateTime='manual')
+            sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,pathOutputEmpty,pathOutputGeoTiff,pathOutputWeb,pathOutputPeta,pathInputPeta,pathVertices,pathLog,pathLanot,pathOutputVistas,dateTime='manual')
             #print("Tiempo de procesamiento: ",round((time.time()-ini)/60,2))
             break
         elif respuesta == 2:
