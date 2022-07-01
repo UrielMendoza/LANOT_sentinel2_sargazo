08c5f42676c10d4245d4d633af0d70de5aa6e2bb
UrielMendoza <urielmendozacastillo@gmail.com>
2022-07-01 13:32:45 -0500

    Update sargazo_manual.py

--- bin/sargazo_manual.py
+++ bin/sargazo_manual.py
@@ -49,9 +49,9 @@ if __name__ == "__main__":
 
         if respuesta == 1:
             #ini = time.time()
-            descargaImagenes(pathTmp,dateTime='manual')
-            imagenL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,pathOutputEmpty,pathOutputGeoTiff,pathOutputWeb,pathOutputPeta,pathInputPeta,pathVertices,pathLog,pathLanot,pathOutputVistas,dateTime='manual')
-            mosaicoL2A(pathTmp,pathInputL1C,pathOutputGeoTiff,pathOutputPeta,pathOutputWeb,dateTime='automaticoTile')
+            #descargaImagenes(pathTmp,dateTime='manual')
+            #imagenL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,pathOutputEmpty,pathOutputGeoTiff,pathOutputWeb,pathOutputPeta,pathInputPeta,pathVertices,pathLog,pathLanot,pathOutputVistas,dateTime='manual')
+            #mosaicoL2A(pathTmp,pathInputL1C,pathOutputGeoTiff,pathOutputPeta,pathOutputWeb,dateTime='automaticoTile')
             sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,pathOutputEmpty,pathOutputGeoTiff,pathOutputWeb,pathOutputPeta,pathInputPeta,pathVertices,pathLog,pathLanot,pathOutputVistas,dateTime='manual')
             #print("Tiempo de procesamiento: ",round((time.time()-ini)/60,2))
             break
