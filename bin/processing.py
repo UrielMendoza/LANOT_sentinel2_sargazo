d1dccf7ebcc9aa38d99a1484a4c4108509543e6a
UrielMendoza <urielmendozacastillo@gmail.com>
2022-08-01 01:44:44 -0500

    Update processing.py

--- bin/processing.py
+++ bin/processing.py
@@ -373,7 +373,7 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,path
                         totalSar = '0'
                         print('6. Añadiendo a la base de datos log...')
                         tproc = round((time.time()-iniTProc)/60,2)
-                        processing_sentinel2.agregaNoSargazoDB(archivol2,archivoProc,fecha,tile,banderaSar_log,totalSar,str(porcNube),'',str(tproc))
+                        processing_sentinel2.agregaNoSargazoDB(archivol2,archivoProc,fecha,tile,banderaSar_log,totalSar,str(porcNube),str(porcNube),str(tproc))
                         # SE PASA A OTRA IMAGEN
                         os.system('rm -r '+pathTmp+'*.tif')
                         os.system('rm -r '+pathTmp+'*.zip')
