c063652c81a803818be9fc798414e0b421ae0624
UrielMendoza <urielmendozacastillo@gmail.com>
2022-09-07 18:46:59 -0500

    Update processing.py

--- bin/processing.py
+++ bin/processing.py
@@ -213,8 +213,8 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,path
     bandas20m = ('B02','B03','B04','B05','B8A','B11','B12','SCL')
     bandas10m = ['B08']
 
-    print('DATETIME:'+dateTime)
     if dateTime != 'automaticoTile' or dateTime != 'semiManualTile':
+        print('DATETIME:'+dateTime)
         tiles = base.tiles[region]
     else:
         tiles = tiles.split()
