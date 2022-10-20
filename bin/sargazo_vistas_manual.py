859958ab89577ae17be04569860dfe64a46f5e6f
UrielMendoza <urielmendozacastillo@gmail.com>
2022-10-19 23:15:37 -0500

    Update sargazo_vistas_manual.py

--- bin/sargazo_vistas_manual.py
+++ bin/sargazo_vistas_manual.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     fechas = list(set(fechas))
     fechas.sort()    
     # Manual
-    fechas = ['20221015']
+    fechas = ['20221015T143729']
     print(fechas)
     for fecha in fechas:
         # Mosaicos
