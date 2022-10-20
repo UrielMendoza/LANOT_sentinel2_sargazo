57fc1c67410bf0d456fe3cbfe05caa85d018b284
UrielMendoza <urielmendozacastillo@gmail.com>
2022-10-20 13:41:27 -0500

    Update sargazo_vistas_manual.py

--- bin/sargazo_vistas_manual.py
+++ bin/sargazo_vistas_manual.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
         # Mosaicos
         #createMosaicLatest(fecha,'TC',pathOutputGeoTiff,pathOutputPeta,pathOutputWeb)
         createMosaicFecha(fecha,'TC','TC_2',pathOutputGeoTiff,pathOutputPeta,pathOutputWeb,pathTmp)
-        createMosaicFecha(fecha,'sargazo','sargazo_2',pathOutputGeoTiff,pathOutputPeta,pathOutputWeb,pathTmp)
+        #createMosaicFecha(fecha,'sargazo','sargazo_2',pathOutputGeoTiff,pathOutputPeta,pathOutputWeb,pathTmp)
         # Vistas
         #sargazo_vistas.vistasSargazo(fecha, region, pathTmp, pathOutputGeoTiff, pathVertices, pathOutputVistas, pathLanot, pathOutputPeta, pathOutputWeb)
         #os.system('python3 sargazo_vistas_vertices.py '+fecha+' s1')
