0736a676a84674c3f937ca4b79ce50e51d8c6216
UrielMendoza <urielmendozacastillo@gmail.com>
2022-11-17 13:24:39 -0600

    prueba roi

--- install.sh
+++ install.sh
@@ -32,6 +32,7 @@ wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/playas_UTM16
 wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/cuerpos_agua_UTM16N_20m_2021.geojson;
 wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/land_UTM16N_20m_distance.geojson;
 wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/land_UTM16N_20m_2021_b500m.geojson;
+wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/land_1_UTM20N_2021.geojson;
 #wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/land_sargazo_UTM16N_20m.tif;
 #wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/land_sargazo_UTM16N_20m_b2km.tif;
 #wget -P ./data/masks/ http://132.247.103.145/tmp/sargazo/data/masks/land_sargazo_UTM16N_20m_b5km.tif;
