e1b7c0bf62f9ad81c0f63bbc3a730f6d4b997953
UrielMendoza <46738566+UrielMendoza@users.noreply.github.com>
2022-12-05 13:34:27 -0600

    Update processing.py

--- bin/processing.py
+++ bin/processing.py
@@ -279,7 +279,7 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,path
     for tileDir in tilesDirs:        
         try:
             if dateTime == 'semiManual' or dateTime == 'semiManualTile':
-                anioProc = '*L1C_2018*T16QCD*'
+                anioProc = '*L1C_2018*T16QDF*'
                 #print('AQUI: '+tileDir+'/*'+anioProc+'*')
                 archivos = processing_sentinel2.listaArchivos(tileDir+'/*'+anioProc+'*')
             else:
