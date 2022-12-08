421d0c96023db00a7835e264be403e810b6f9c8d
UrielMendoza <urielmendozacastillo@gmail.com>
2022-12-08 11:09:33 -0600

    Update processing.py

--- bin/processing.py
+++ bin/processing.py
@@ -49,7 +49,7 @@ def automaticoTile():
 
 def automatico():
     # Se le resta un dia, porque el servidor en UTC
-    daysDelta = 2
+    daysDelta = 1
     start_date = datetime.datetime.now() - datetime.timedelta(days=daysDelta)
     end_date = datetime.datetime.now()  - datetime.timedelta(days=daysDelta)
     region = "sargazo_3"
