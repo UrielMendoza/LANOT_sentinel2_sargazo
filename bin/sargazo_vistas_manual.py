7060057baeccf1e6bcfe2dfcaafdf4043515767d
UrielMendoza <46738566+UrielMendoza@users.noreply.github.com>
2023-02-15 14:59:07 -0600

    Update sargazo_vistas_manual.py

--- bin/sargazo_vistas_manual.py
+++ bin/sargazo_vistas_manual.py
@@ -26,17 +26,17 @@ if __name__ == "__main__":
     fechas = list(set(fechas))
     fechas.sort()    
     # Manual
-    fechas = ['20221219T143731']
+    fechas = ['20230205T161449']
     print(fechas)
     for fecha in fechas:
         # Mosaicos
         #createMosaicLatest(fecha,'TC',pathOutputGeoTiff,pathOutputPeta,pathOutputWeb)
-        createMosaicFecha(fecha,'TC','TC_2',pathOutputGeoTiff,pathOutputPeta,pathOutputWeb,pathTmp)
-        createMosaicFecha(fecha,'sargazo','sargazo_2',pathOutputGeoTiff,pathOutputPeta,pathOutputWeb,pathTmp)
+        #createMosaicFecha(fecha,'TC','TC_2',pathOutputGeoTiff,pathOutputPeta,pathOutputWeb,pathTmp)
+        #createMosaicFecha(fecha,'sargazo','sargazo_2',pathOutputGeoTiff,pathOutputPeta,pathOutputWeb,pathTmp)
         # Vistas
         #sargazo_vistas.vistasSargazo(fecha, region, pathTmp, pathOutputGeoTiff, pathVertices, pathOutputVistas, pathLanot, pathOutputPeta, pathOutputWeb)
-        #os.system('python3 sargazo_vistas_vertices.py '+fecha+' s1')
-        #os.system('python3 sargazo_vistas_vertices.py '+fecha+' s2')
+        os.system('python3 sargazo_vistas_vertices.py '+fecha+' s1')
+        os.system('python3 sargazo_vistas_vertices.py '+fecha+' s2')
         #uneCentroides(pathVertices+'sargazo_centroides/',fecha,pathVertices+'sargazo_centroides/s1/',pathOutputPeta,pathOutputWeb)
 
         # Borra Tmp
