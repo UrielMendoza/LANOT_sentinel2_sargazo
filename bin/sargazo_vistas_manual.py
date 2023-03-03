901a576bd9e709294bd77d2a78f3a3df4ae32483
UrielMendoza <46738566+UrielMendoza@users.noreply.github.com>
2023-03-03 11:19:45 -0600

    Update sargazo_vistas_manual.py

--- bin/sargazo_vistas_manual.py
+++ bin/sargazo_vistas_manual.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
         #os.system('python3 sargazo_vistas_vertices.py '+fecha+' s1')
         #os.system('python3 sargazo_vistas_vertices.py '+fecha+' s2')
         #uneCentroides(pathVertices+'sargazo_centroides/',fecha,pathVertices+'sargazo_centroides/s1/',pathOutputPeta,pathOutputWeb)
-        uneVectorial(4326,'sargazo_segmentados',pathVertices+'sargazo_segmentados/','2023-03-02',pathVertices+'sargazo_segmentados/s1/',pathOutputPeta,pathOutputWeb)
+        uneVectorial(4326,'sargazo_segmentados',pathVertices+'sargazo_segmentados/',fecha,pathVertices+'sargazo_segmentados/s1/',pathOutputPeta,pathOutputWeb)
 
         # Borra Tmp
         os.system('rm -r '+pathTmp+'*.tif')
