4e6adb81b2e335579b5f43daeb48dad03e5134b6
UrielMendoza <urielmendozacastillo@gmail.com>
2023-03-21 12:43:23 -0600

    Update processing.py

--- bin/processing.py
+++ bin/processing.py
@@ -579,13 +579,13 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,path
                     processing_sentinel2.enviaMail(fecha,tile,traceback.format_exc().replace("'",""))
                     continue
 
-                #finally:
+                finally:
                 # BORRA BASURA
                     #os.system('rm -r '+pathTmp+'*.tif')
-                    #os.system('rm -r '+pathTmp+'*json')
-                    #os.system('rm -r '+pathTmp+'*.csv')
-                    #os.system('rm -r '+pathTmp+'*.zip')
-                    #os.system('rm -r '+pathTmp+'*.SAFE')
+                    os.system('rm -r '+pathTmp+'*json')
+                    os.system('rm -r '+pathTmp+'*.csv')
+                    os.system('rm -r '+pathTmp+'*.zip')
+                    os.system('rm -r '+pathTmp+'*.SAFE')
                     #if dateTime == 'automatico' or dateTime == 'manual':
                     #    os.system('rm -r '+pathTmp+'*')
                 
@@ -663,8 +663,8 @@ def sargazoL2A(pathInputL1C,pathInput,pathOutput,pathTmp,pathLM,pathSen2cor,path
         
     # BORRA DIR DESCARGA
     # NO DESCOMENTAR EN SEMIMANUAL PORQUE BORRA IMAGENES
-    #if dateTime == 'automatico' or dateTime == 'manual' or dateTime == 'automaticoTile':
-       #os.system('rm -r '+pathTmp+'*')
+    if dateTime == 'automatico' or dateTime == 'manual' or dateTime == 'automaticoTile':
+       os.system('rm -r '+pathTmp+'*')
 
 
 
