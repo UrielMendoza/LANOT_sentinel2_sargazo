31ebe7e18acf1acee296a504464fcdb56d320fe1
UrielMendoza <46738566+UrielMendoza@users.noreply.github.com>
2023-03-22 12:04:58 -0600

    Update sargazo_vistas_manual.py

--- bin/sargazo_vistas_manual.py
+++ bin/sargazo_vistas_manual.py
@@ -31,14 +31,14 @@ if __name__ == "__main__":
     for fecha in fechas:
         # Mosaicos
         #createMosaicLatest(fecha,'TC',pathOutputGeoTiff,pathOutputPeta,pathOutputWeb)
-        #createMosaicFecha(fecha,'TC','TC',pathOutputGeoTiff,pathOutputPeta,pathOutputWeb,pathTmp)
-        #createMosaicFecha(fecha,'sargazo','sargazo',pathOutputGeoTiff,pathOutputPeta,pathOutputWeb,pathTmp)
+        createMosaicFecha(fecha,'TC','TC',pathOutputGeoTiff,pathOutputPeta,pathOutputWeb,pathTmp)
+        createMosaicFecha(fecha,'sargazo','sargazo',pathOutputGeoTiff,pathOutputPeta,pathOutputWeb,pathTmp)
         # Vistas
-        #sargazo_vistas.vistasSargazo(fecha, region, pathTmp, pathOutputGeoTiff, pathVertices, pathOutputVistas, pathLanot, pathOutputPeta, pathOutputWeb)
-        #os.system('python3 sargazo_vistas_vertices.py '+fecha+' s1')
-        #os.system('python3 sargazo_vistas_vertices.py '+fecha+' s2')
+        sargazo_vistas.vistasSargazo(fecha, region, pathTmp, pathOutputGeoTiff, pathVertices, pathOutputVistas, pathLanot, pathOutputPeta, pathOutputWeb)
+        os.system('python3 sargazo_vistas_vertices.py '+fecha+' s1')
+        os.system('python3 sargazo_vistas_vertices.py '+fecha+' s2')
         uneVectorial(4326,'sargazo_centroides',pathVertices+'sargazo_centroides/',fecha,pathVertices+'sargazo_centroides/s1/',pathOutputPeta,pathOutputWeb)
-        #uneVectorial(4326,'sargazo_segmentados',pathVertices+'sargazo_segmentados/',fecha,pathVertices+'sargazo_segmentados/s1/',pathOutputPeta,pathOutputWeb)
+        uneVectorial(4326,'sargazo_segmentados',pathVertices+'sargazo_segmentados/',fecha,pathVertices+'sargazo_segmentados/s1/',pathOutputPeta,pathOutputWeb)
 
         # Borra Tmp
         os.system('rm -r '+pathTmp+'*.tif')
