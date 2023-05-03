224d1b19b174a488db12fbb3155987d84e6031ec
UrielMendoza <urielmendozacastillo@gmail.com>
2023-05-02 23:00:09 -0600

    Update sargazo_vistas_manual.py

--- bin/sargazo_vistas_manual.py
+++ bin/sargazo_vistas_manual.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     fechas = list(set(fechas))
     fechas.sort()    
     # Manual
-    fechas = ['20230411T160831']
+    fechas = ['20230501T160821']
     print(fechas)
     for fecha in fechas:
         # Mosaicos
