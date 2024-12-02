88d5cedd5a90abcb5280cdeccffad3be5d94b175
UrielMendoza <46738566+UrielMendoza@users.noreply.github.com>
2024-12-01 22:44:59 -0600

    Update processing_sentinel2.py

--- bin/processing_sentinel2.py
+++ bin/processing_sentinel2.py
@@ -831,7 +831,7 @@ def afaiBinNumpy(pathInput):
     
     # Umbralizar el índice AFAI
     # Asigna 1 a valores mayores a 0.06 y dentro del rango >= 0.025
-    sargazoBin = np.where(afai >= 0.06, 1, 0)
+    sargazoBin = np.where(afai >= 0.005, 1, 0)
 
     # Imprime los datos necesarios para la detección de sargazo
     print('AFAI:', afai.min(), afai.max(), afai.mean())
